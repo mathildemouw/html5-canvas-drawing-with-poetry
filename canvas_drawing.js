61d1e04377e4d8a72a6cbac64af1b0527436a905
Mathilde Mouw <nihaomouw@mac.com>
2014-04-25 10:47:04 -0700

    Create RandomCanvas class

--- canvas_drawing.js
+++ canvas_drawing.js
@@ -1,17 +1,21 @@
 window.onload = function(){
 
-function draw(canvasId) {
-	my_canvas = document.getElementById(canvasId); //ack, need var or no?  I forgot everything!
-	my_context = my_canvas.getContext("2d");
+function RandomCanvas(canvasId){
+	this.canvas = document.getElementById(canvasId)
+	this.context = this.canvas.getContext("2d")
 }
+// function draw(canvasId) {
+// 	my_canvas = document.getElementById(canvasId); //ack, need var or no?  I forgot everything!
+// 	my_context = my_canvas.getContext("2d");
+// }
 
-draw("yo");
-// later, could make an awesome function to pass in any canvas and give it a super cool gradient
-var snazzyGradient = my_context.createLinearGradient(0, 0, 290, 0);
+var my_canvas = new RandomCanvas("yo");
+
+var snazzyGradient = my_canvas.context.createLinearGradient(0, 0, 290, 0);
 snazzyGradient.addColorStop(0, "pink");
 snazzyGradient.addColorStop(1, "blue");
-my_context.fillStyle = snazzyGradient;
-my_context.fillRect(0, 0, 290, 150)
+my_canvas.context.fillStyle = snazzyGradient;
+my_canvas.context.fillRect(0, 0, 290, 150)
 
 
 }
