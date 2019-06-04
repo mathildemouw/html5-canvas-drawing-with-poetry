dc8fc3b50db5de3853a0cf8f65f86082a404e409
Mathilde <nihaomouw@mac.com>
2019-06-03 22:10:05 -0700

    Rename random_canvas.html, fix type, show user what they entered

--- canvas_drawing.js
+++ canvas_drawing.js
@@ -1,24 +1,29 @@
 window.onload = function () {
 
-
-function RandomCanvas ( canvasId ) {
-	this.canvas = document.getElementById( canvasId )
+function RandomCanvas ( drawhere ) {
+	this.canvas = document.getElementById( drawhere )
 	this.context = this.canvas.getContext( "2d" )
 };
 
 RandomCanvas.prototype = {
 	drawWithWords: function ( stanza ) {
-			this.context.strokeStyle = "#ff00dd";
+			this.setLineColor();
 			this.context.moveTo( stanza[ 0 ].charCodeAt(), stanza[ 0 ].charCodeAt() );
 		for ( var x = stanza.length - 1; x >= 0; x-- ) {
 			x_coord = stanza[ x ].charCodeAt();
 			this.context.lineTo( x_coord, (Math.random()*150) );
 			this.context.stroke();
 		}
+		document.getElementById( "youdrew" ).innerHTML = stanza;
 	},
+
+	setLineColor: function () {
+		this.context.strokeStyle = "#FF1493";
+	},
+
 };
 
-var my_canvas = new RandomCanvas ( "yo" ) ;
+var my_canvas = new RandomCanvas ( "drawhere" ) ;
 
 var myStanza =  prompt( "What words would you like to scribble? Example: 'This is just to say'" ) ;
 my_canvas.drawWithWords( myStanza );
